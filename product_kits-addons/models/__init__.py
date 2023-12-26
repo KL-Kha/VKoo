c279dd638708807a5d77cef4168629db7c889b2a
WELCOME <trungvyvo123@gmail.com>
2023-12-27 03:37:43 +0700

    VKoo Committing

--- product_kits-addons/models/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import x_product_template
-from . import x_res_company
-from . import x_res_config_settings
--- product_kits-addons/models/x_res_company.py
+++
@@ -1,6 +0,0 @@
-from odoo import fields, models,_
-
-class ResCompany(models.Model):
-    _inherit = 'res.company'
-
-    x_anfrage_modus_mode = fields.Boolean(string='Anfrage Modus / RQF Mode', default=False)
