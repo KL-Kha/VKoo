9d2a314787d29f1ba7101f76122a20d57ab4b2c6
WELCOME <trungvyvo123@gmail.com>
2023-12-27 03:37:25 +0700

    VKoo Committing

---
+++ poweron_product_kits_addons/__manifest__.py
@@ -0,0 +1,30 @@
+{
+    'name': "PowerOn Product Kits Addons",
+    'summary': "PowerOn Product Kits Management Addons",
+    'version': '15.0.1.0.0',
+    'author': "simplify-m110",
+    'category': 'Sales',
+    'website': 'https://simplify-erp.com',
+    'license': 'LGPL-3',
+    'depends': ['base','product','product_kits-main'],
+    'data': [
+        #data
+        "data/ir_access.xml",
+        "data/automatic_action.xml",
+
+        # security
+        # 'security/ir.model.access.csv',
+
+        # views
+        'views/x_product_template.xml',
+        'views/x_res_config_settings_views.xml',
+
+    ],
+    # 'assets': {
+    #     'web.assets_frontend': [
+    #         'product_kits-addons/static/src/js/x_website_sale.js',
+    #     ],
+    # },
+    'auto_install': False,
+    'installable': True,
+}
