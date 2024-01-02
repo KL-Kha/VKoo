e152bfbff06bca7a997d3a0b0a63140d0029fe25
Trần Bảo Kha <baokha0009@gmail.com>
2024-01-03 01:16:25 +0700

    update

--- erp/poweron_website_sale/__manifest__.py
+++ erp/poweron_website_sale/__manifest__.py
@@ -7,9 +7,12 @@
     "summary": "Adjust content on website sale",
     "depends": ["website","website_sale"],
     "data": [
-        "data/ir_asset.xml",
         "views/website_sale_template.xml",
     ],
-    
+    'assets': {
+        'web.assets_frontend': [
+            'poweron_website_sale/static/src/scss/user_custom_rules.scss',
+        ],
+    },
     "installable": True,
 }
