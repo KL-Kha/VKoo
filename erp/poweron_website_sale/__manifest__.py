fabbf68146c7c5c607562f1d51bcc734ab589616
Trần Bảo Kha <baokha0009@gmail.com>
2024-01-02 23:42:06 +0700

    update

--- erp/poweron_website_sale/__manifest__.py
+++ erp/poweron_website_sale/__manifest__.py
@@ -5,9 +5,16 @@
     "author": "Poweron Software",
     "license": "AGPL-3",
     "summary": "Adjust content on website sale",
-    "depends": ["website_sale"],
+    "depends": ["website","website_sale"],
     "data": [
+        "data/ir_asset.xml",
         "views/website_sale_template.xml",
     ],
+    'assets': {
+        'web.assets_editor': [
+            ('replace', 'website/static/src/components/ace_editor/ace_editor.js', 'poweron_website_sale/static/src/js/ace_editor.js'),
+        ],
+    },
+    
     "installable": True,
 }
