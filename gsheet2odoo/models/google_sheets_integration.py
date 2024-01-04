7243819c2e72aabe477717134ef80ad2dd2474b8
WELCOME <trungvyvo123@gmail.com>
2024-01-04 16:42:51 +0700

    VKoo Committing Erp

--- gsheet2odoo/models/google_sheets_integration.py
+++ gsheet2odoo/models/google_sheets_integration.py
@@ -7,8 +7,8 @@ from urllib.parse import urlparse, parse_qs
 import pandas as pd
 import re
 
-
 _logger = logging.getLogger(__name__)
+
 
 class GoogleSheetsIntegration(models.Model):
     _name = 'google.sheets.integration'
@@ -25,10 +25,10 @@ class GoogleSheetsIntegration(models.Model):
             return parse_qs(parsed_url.query)['key'][0]
         else:
             raise ValidationError(_("Invalid Google Sheets URL"))
-        
+
     def find_or_create_product(self, product_data):
         if product_data['default_code'] and product_data['name']:
-        # Search for existing product with the given reference or name
+            # Search for existing product with the given reference or name
             existing_product = self.env['product.product'].search([
                 '|', ('default_code', '=', product_data['default_code']), ('name', '=', product_data['name'])
             ], limit=1)
@@ -44,7 +44,7 @@ class GoogleSheetsIntegration(models.Model):
                 return existing_product
         else:
             return False
-        
+
     def create_sale_order(self, ref_id_sheet, customer_id_sheet):
         existing_customer = self.env['res.partner'].search([('ref', '=', ref_id_sheet)])
 
@@ -69,7 +69,6 @@ class GoogleSheetsIntegration(models.Model):
         self.env.cr.commit()
         return new_sale_order
 
-
     @api.model
     def get_google_sheet_data(self):
         scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
@@ -87,22 +86,31 @@ class GoogleSheetsIntegration(models.Model):
         worksheet_value = worksheet.get_all_values()
         worksheet_value_correct = worksheet_value[4:]
 
-        data_group_month = pd.DataFrame(worksheet_value_correct).groupby([1,2])
-        
-        for key,vals in data_group_month.groups.items():
+        data_group_month = pd.DataFrame(worksheet_value_correct).groupby([1, 2])
+
+        for key, vals in data_group_month.groups.items():
             group_by_data_user = data_group_month.get_group(key)
-            sale_order_id = self.create_sale_order(key[1],group_by_data_user[3][vals[0]])
+            sale_order_id = self.create_sale_order(key[1], group_by_data_user[3][vals[0]])
             for i in group_by_data_user.index:
                 PRODUCT_SHEET_RANGES = [
-                    {'product_uom_qty': group_by_data_user[4][i], 'default_code': group_by_data_user[7][i], 'name': group_by_data_user[8][i]},
-                    {'product_uom_qty': group_by_data_user[9][i], 'default_code': group_by_data_user[10][i], 'name': group_by_data_user[11][i]},
-                    {'product_uom_qty': group_by_data_user[12][i], 'default_code': group_by_data_user[13][i], 'name': group_by_data_user[14][i]},
-                    {'product_uom_qty': group_by_data_user[15][i], 'default_code': group_by_data_user[16][i], 'name': group_by_data_user[17][i]},
-                    {'product_uom_qty': group_by_data_user[18][i], 'default_code': group_by_data_user[19][i], 'name': group_by_data_user[20][i]},
-                    {'product_uom_qty': group_by_data_user[21][i], 'default_code': group_by_data_user[22][i], 'name': group_by_data_user[23][i]},
-                    {'product_uom_qty': group_by_data_user[24][i], 'default_code': group_by_data_user[25][i], 'name': group_by_data_user[26][i]},
-                    {'product_uom_qty': group_by_data_user[27][i], 'default_code': group_by_data_user[28][i], 'name': group_by_data_user[29][i]},
-                    {'product_uom_qty': group_by_data_user[30][i], 'default_code': group_by_data_user[31][i], 'name': group_by_data_user[32][i]},
+                    {'product_uom_qty': group_by_data_user[4][i], 'default_code': group_by_data_user[7][i],
+                     'name': group_by_data_user[8][i]},
+                    {'product_uom_qty': group_by_data_user[9][i], 'default_code': group_by_data_user[10][i],
+                     'name': group_by_data_user[11][i]},
+                    {'product_uom_qty': group_by_data_user[12][i], 'default_code': group_by_data_user[13][i],
+                     'name': group_by_data_user[14][i]},
+                    {'product_uom_qty': group_by_data_user[15][i], 'default_code': group_by_data_user[16][i],
+                     'name': group_by_data_user[17][i]},
+                    {'product_uom_qty': group_by_data_user[18][i], 'default_code': group_by_data_user[19][i],
+                     'name': group_by_data_user[20][i]},
+                    {'product_uom_qty': group_by_data_user[21][i], 'default_code': group_by_data_user[22][i],
+                     'name': group_by_data_user[23][i]},
+                    {'product_uom_qty': group_by_data_user[24][i], 'default_code': group_by_data_user[25][i],
+                     'name': group_by_data_user[26][i]},
+                    {'product_uom_qty': group_by_data_user[27][i], 'default_code': group_by_data_user[28][i],
+                     'name': group_by_data_user[29][i]},
+                    {'product_uom_qty': group_by_data_user[30][i], 'default_code': group_by_data_user[31][i],
+                     'name': group_by_data_user[32][i]},
                 ]
                 for product_data in PRODUCT_SHEET_RANGES:
                     product_id = self.find_or_create_product(product_data)
@@ -110,7 +118,8 @@ class GoogleSheetsIntegration(models.Model):
                         product_vals = {
                             'order_id': sale_order_id.id,
                             'product_id': product_id.id,
-                            'product_uom_qty': product_data['product_uom_qty'] if product_data['product_uom_qty'] else 0,
+                            'product_uom_qty': product_data['product_uom_qty'] if product_data[
+                                'product_uom_qty'] else 0,
                             # Add other relevant fields as needed
                         }
                         self.env['sale.order.line'].create(product_vals)
