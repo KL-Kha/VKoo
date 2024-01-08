03f1db8e8aa57a5ac7c7c5e31d7f5bc82c089673
Trần Bảo Kha <baokha0009@gmail.com>
2024-01-09 01:04:21 +0700

    update poweron_custom_res_partner

---
+++ poweron_custom_res_partner/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': "Poweron Custom Res Partner",
+    'version': '1.0',
+    'author': "VKoo",
+    'company': "PowerOn | Wapsol GmbH",
+    'depends': ['base','account'],
+    'data': [
+        'views/account_report.xml',
+
+        'views/res_partner_inherit.xml',
+    ],
+}
